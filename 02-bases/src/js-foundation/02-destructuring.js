7cc2f700349aaf1b51b0f48b5a78153974515925
Luis Daniel Ortiz <ls.ortizdanieldeveloper@gmail.com>
2024-08-06 13:29:20 -0600

    agregando depuracion

--- 02-bases/src/js-foundation/02-destructuring.js
+++ 02-bases/src/js-foundation/02-destructuring.js
@@ -5,7 +5,7 @@ const { SHELL, HOMEBREW_CELLAR } = process.env
 
 console.table({SHELL,HOMEBREW_CELLAR});
 
-const characters = ['Flash', 'Superman', 'Batman'];
-const [, __, batman] = characters;
+const characters = ['Flash', 'Superman', 'WonderWoman','Batman'];
+const [, ,__, batman] = characters;
 
-console.log(batman)
+// console.log(batman);
