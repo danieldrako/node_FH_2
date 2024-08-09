24b8590a9ba11db93af961f3ece187dcf9baf0f5
Luis Daniel Ortiz <ls.ortizdanieldeveloper@gmail.com>
2024-08-09 15:56:38 -0600

    agregando arrow y callbacks

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,7 +1,14 @@
-const {emailTemplate}= require('./js-foundation/01-template');
-require('./js-foundation/02-destructuring')
+// const {emailTemplate}= require('./js-foundation/01-template');
+// require('./js-foundation/02-destructuring')
+const {getUserById} = require('./js-foundation/03-callbacks');
 
+const id = 1;
 
+getUserById( id, (error,user) =>{
+  if(error) throw new Error(error);
+
+  console.log(user);
+});
 
 // console.log(emailTemplate)
 
